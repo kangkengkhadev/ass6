6f1f0d3b8914403cc952e0b44080d55d216a309a
Machigar.O <machigar.o@cp.eng.chula.ac.th>
2024-03-03 22:23:24 +0700

    Initial Commit

---
+++ __tests__/case2.test.tsx
@@ -0,0 +1,60 @@
+import '@testing-library/jest-dom'
+import { render, screen, within, waitFor, fireEvent } from '@testing-library/react'
+import CardPanel from '@/components/CardPanel'
+
+describe('CardPanel', () => {
+  it('CardPanel support ratings', async () => {
+    render(<CardPanel/>)
+
+    const chulaRating = screen.getByTestId("Chulalongkorn Hospital Rating")
+    const rajavithiRating = screen.getByTestId("Rajavithi Hospital Rating")
+    const thammasatRating = screen.getByTestId("Thammasat University Hospital Rating")
+
+    expect(chulaRating).toBeInTheDocument
+    expect(rajavithiRating).toBeInTheDocument
+    expect(thammasatRating).toBeInTheDocument
+
+    const chulaFour = within(chulaRating).getByRole('radio', {name: "4 Stars"}) as HTMLElement
+    expect(chulaFour).toBeInTheDocument
+    fireEvent.click(chulaFour)
+    await waitFor(() => {
+      const chulaDisplay = screen.getByTestId("Chulalongkorn Hospital")
+      expect(chulaDisplay).toBeInTheDocument
+      expect(chulaDisplay.innerHTML).toMatch(/4/i)
+    })
+
+    const rajavithiThree = within(rajavithiRating).getByRole('radio', {name: "3 Stars"}) as HTMLElement
+    expect(rajavithiThree).toBeInTheDocument
+    fireEvent.click(rajavithiThree)
+    await waitFor(() => {
+      const rajavithiDisplay = screen.getByTestId("Rajavithi Hospital")
+      expect(rajavithiDisplay).toBeInTheDocument
+      expect(rajavithiDisplay.innerHTML).toMatch(/3/i)
+      expect(rajavithiThree).toBeChecked()
+    })
+
+    const rajavithiDisplay = screen.getByTestId("Rajavithi Hospital")
+    expect(rajavithiDisplay).toBeInTheDocument
+    fireEvent.click(rajavithiDisplay)
+    await waitFor(() => {
+      const rajavithiDisplay = screen.queryAllByTestId("Rajavithi Hospital")
+      expect(rajavithiDisplay).toHaveLength(0)
+
+      const rajavithiOne = within(rajavithiRating).getByRole('radio', {name: "1 Star"}) as HTMLElement
+      const rajavithiTwo = within(rajavithiRating).getByRole('radio', {name: "2 Stars"}) as HTMLElement
+      const rajavithiThree = within(rajavithiRating).getByRole('radio', {name: "3 Stars"}) as HTMLElement
+      const rajavithiFour = within(rajavithiRating).getByRole('radio', {name: "4 Stars"}) as HTMLElement
+      const rajavithiFive = within(rajavithiRating).getByRole('radio', {name: "5 Stars"}) as HTMLElement
+      expect(rajavithiOne).not.toBeChecked()
+      expect(rajavithiTwo).not.toBeChecked()
+      expect(rajavithiThree).not.toBeChecked()
+      expect(rajavithiFour).not.toBeChecked()
+      expect(rajavithiFive).not.toBeChecked()
+    })
+
+  } //end it
+  )
+})
+
+
+
